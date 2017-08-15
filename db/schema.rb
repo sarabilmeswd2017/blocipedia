6db80cc441aedf63fe664860c9637672a5a2dad8
Sara Bilmes <saraesrig@gmail.com>
2017-08-15 13:08:55 +0300

    User-sign-up:not complete

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20170815064251) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   :confirmation_token
+    t.datetime :confirmed_at
+    t.datetime :confirmation_sent_at
+    t.string   :unconfirmed_email # Only if using reconfirmable
+
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   let(:user) { User.create!(email: "user@bloccit.com", password: "password") }
+
+   it { is_expected.to validate_presence_of(:email) }
+   it { is_expected.to validate_uniqueness_of(:email) }
+   it { is_expected.to validate_length_of(:email).is_at_least(3) }
+   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
+
+   it { is_expected.to validate_presence_of(:password) }
+   it { is_expected.to have_secure_password }
+   it { is_expected.to validate_length_of(:password).is_at_least(6) }
+
+   describe "attributes" do
+     it "should have email attribute" do
+       expect(user).to have_attributes(email: user.email)
+     end
 end
