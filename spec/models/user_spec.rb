5daf3d2a4cef8d6278bae7ca123fdc41cc35c424
Sara Bilmes <saraesrig@gmail.com>
2017-08-16 09:37:39 +0300

    User Sign Up- confirmation email not working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-   let(:user) { User.create!(email: "user@bloccit.com", password: "password") }
-
-   it { is_expected.to validate_presence_of(:email) }
-   it { is_expected.to validate_uniqueness_of(:email) }
-   it { is_expected.to validate_length_of(:email).is_at_least(3) }
-   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
-
-   it { is_expected.to validate_presence_of(:password) }
-   it { is_expected.to have_secure_password }
-   it { is_expected.to validate_length_of(:password).is_at_least(6) }
-
-   describe "attributes" do
-     it "should have email attribute" do
-       expect(user).to have_attributes(email: user.email)
-     end
+  
 end
