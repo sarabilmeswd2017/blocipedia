2add51ac6eecabdd81b717ef2adbb3730fb9de40
Sara Bilmes <saraesrig@gmail.com>
2017-08-22 20:33:54 +0300

    fixing name attempt 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,22 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-
-
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ protected
+
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+ end
+
   private
-
-  #def user_not_authorized
-    #flash[:alert] = "You are not authorized to perform this action."
-    #redirect_to(request.referrer || root_path)
-  #end
-
 
   def user_not_authorized
    flash[:alert] = "You are not authorized to perform this action."
    redirect_to(request.referrer || root_path)
- end
-  #after_action :verify_authorized, except: :index
-  #after_action :verify_policy_scoped, only: :index
+  end
+
+
 end
