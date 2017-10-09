b0f58db296f0e6ab46cf21eeb63d2a67ff87df66
Sara Bilmes <saraesrig@gmail.com>
2017-10-09 20:38:04 +0300

    added click for new-btn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,17 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+var blocmetrics= {};
+blocmetrics.report = function(eventName){
+  var event = {event: { name: eventName }};
+  var request = new XMLHttpRequest();
+  request.open("POST", "http://localhost:3000/api/events", true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify(event));
+};
+
+$(document).on('turbolinks:load', function() {
+  $('.new-btn').click(function() {
+    blocmetrics.report('clicked on new wiki');
+  })
+})
